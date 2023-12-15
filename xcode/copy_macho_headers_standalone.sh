c58782f11f2f4b47fb864d3816d5d8dff3e408a3
Apple OSS Distributions <91980991+AppleOSSDistributions@users.noreply.github.com>
2023-12-15 20:52:34 +0000

    cctools-1009.3
    Imported from cctools-1009.3.tar.gz

--- xcode/copy_macho_headers_standalone.sh
+++ xcode/copy_macho_headers_standalone.sh
@@ -17,4 +17,15 @@ if [ "${DRIVERKIT}" == 1 ]; then
     cp ${SRCROOT}/include/mach-o/arm64/reloc.h ${DSTROOT}/${PUBLIC_HEADERS_FOLDER_PATH}/arm64/
     mkdir -p ${DSTROOT}/${PUBLIC_HEADERS_FOLDER_PATH}/x86_64
     cp ${SRCROOT}/include/mach-o/x86_64/reloc.h ${DSTROOT}/${PUBLIC_HEADERS_FOLDER_PATH}/x86_64/
+elif [ "${SYSTEM_PREFIX}" != "" ]; then # Exclaves
+    mkdir -p ${DSTROOT}${SYSTEM_PREFIX}/usr/include/mach-o
+    mkdir -p ${DSTROOT}${SYSTEM_PREFIX}/usr/local/include/mach-o
+
+    cp ${SRCROOT}/include/mach-o/fat.h ${DSTROOT}${SYSTEM_PREFIX}/usr/include/mach-o
+    cp ${SRCROOT}/include/mach-o/loader.h ${DSTROOT}${SYSTEM_PREFIX}/usr/include/mach-o
+    cp ${SRCROOT}/include/mach-o/nlist.h ${DSTROOT}${SYSTEM_PREFIX}/usr/include/mach-o
+    cp ${SRCROOT}/include/standalone/getsect.h ${DSTROOT}${SYSTEM_PREFIX}/usr/include/mach-o
+
+    cp ${SRCROOT}/include/modules/mach-o.exclaves.modulemap ${DSTROOT}${SYSTEM_PREFIX}/usr/include/mach-o/module.modulemap
+    cp ${SRCROOT}/include/modules/mach-o.private.modulemap ${DSTROOT}${SYSTEM_PREFIX}/usr/local/include/mach-o/module.modulemap
 fi
